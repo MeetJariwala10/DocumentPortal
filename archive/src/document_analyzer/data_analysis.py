8176122b82381928aec6a619af339e1cfb9c9b6d
Meet Jariwala <meetjari0512@gmail.com>
2025-08-16 22:42:19 +0530

    moved the current src codes into archive folder

---
+++ archive/src/document_analyzer/data_analysis.py
@@ -0,0 +1,67 @@
+import os
+import sys
+from utils.model_loader import ModelLoader
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from model.models import *
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
+from prompt.prompt_library import PROMPT_REGISTRY
+
+class DocumentAnalyzer:
+    """
+    Analyzes documents using a pre-trained model.
+    Automatically logs all actions and supports session-based organization.
+    """
+    def __init__(self):
+        
+        self.log = CustomLogger().get_logger(__name__)
+        
+        try:
+            self.loader = ModelLoader()
+            self.llm = self.loader.load_llm()
+            
+            # Prepare parsers
+            self.parser = JsonOutputParser(pydantic_object=Metadata)
+
+            # NOTE:
+            # Why OutputFixingParser?
+            # First, call your LLM with the normal prompt + “please output valid JSON for this schema.”
+            # Then, if the LLM’s response fails to parse (e.g. missing a bracket, wrong field name, extra text), it will automatically issue a follow‑up prompt to itself like “Hey, fix your last answer so it’s valid JSON matching the specified schema.”
+            # Finally, return you a well‑formed object satisfying your JsonOutputParser’s Pydantic model.
+            self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
+            
+            self.prompt = PROMPT_REGISTRY["document_analysis"]
+            
+            self.log.info("DocumentAnalyzer initialized successfully")
+            
+            
+        except Exception as e:
+            self.log.error(f"Error initializing DocumentAnalyzer: {e}")
+            raise DocumentPortalException("Error in DocumentAnalyzer initialization", sys)
+        
+        
+    
+    def analyze_document(self, document_text:str)-> dict:
+        """
+        Analyze a document's text and extract structured metadata & summary.
+        """
+        try:
+            chain = self.prompt | self.llm | self.fixing_parser
+            
+            self.log.info("Meta-data analysis chain initialized")
+
+            response = chain.invoke({
+                "format_instructions": self.parser.get_format_instructions(),
+                "document_text": document_text
+            })
+
+            self.log.info("Metadata extraction successful", keys=list(response.keys()))
+            
+            return response
+
+        except Exception as e:
+            self.log.error("Metadata analysis failed", error=str(e))
+            raise DocumentPortalException("Metadata extraction failed") from e
+        
+ 
